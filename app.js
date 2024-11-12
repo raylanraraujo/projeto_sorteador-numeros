628e727a030fa4d0abf83ac7ff5e0cebf57b4f36
Raylan Araujo <raylanra@gmail.com>
2024-11-12 15:18:27 -0300

    Inlcuir função para o botão reiniciar

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ function sortear() {
     alterarStatusBotao();
 }
 
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    // sorteados = []; --> na aula não foi incluído nada sobre limpar o array sorteados;
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`;
+    alterarStatusBotao();
+}
 
 function obterNumeroAleatorio(min , max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
