dc787293d52fc9f68e91b582043530abd7f0bac5
Raylan Araujo <raylanra@gmail.com>
2024-11-12 14:44:56 -0300

    Incluir verificação dos números sorteados para que todos sejam diferentes entre si

--- app.js
+++ app.js
@@ -8,12 +8,20 @@ function sortear() {
 
     for (let i = 0; i < quantidade; i++){
         numero = obterNumeroAleatorio(doNumero, ateNumero);
+
+        while(sorteados.includes(numero)){ // para verificar se o número sorteado está dentro do array sorteados
+            numero = obterNumeroAleatorio(doNumero, ateNumero); // para verificar se o 
+        }
         sorteados.push(numero)
     }
-    alert(`Os números sorteado: ${sorteados}`)
+    
+    
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
 }
 
 function obterNumeroAleatorio(min , max) {
    
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
